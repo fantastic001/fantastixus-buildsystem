4d7d7b890c6fce8d827c34d96e3e82e15c137c6c
Stefan Nožinić <stefan@lugons.org>
2020-07-19 01:49:06 +0200

    Modify setup script to better map directories

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ fi
 mkdir -p $THIS_DIR/build
 chmod 777 -R $THIS_DIR/build
 docker run -ti \
-	--volume "$THIS_DIR/layers/:/env/" \
-	--volume "$THIS_DIR/build/:/home/build/" \
-	--volume "$THIS_DIR/init/gosu:/usr/bin/gosu" \
-	yocto:3.1 gosu 1000 /bin/bash
+	--volume "$THIS_DIR/:$THIS_DIR" \
+	-w $THIS_DIR \
+	yocto:3.1 $THIS_DIR/init/gosu 1000 /bin/bash
