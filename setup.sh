e0f4d3087170616afd1b3306451b48fd01a6994c
Stefan Nožinić <stefan@lugons.org>
2020-07-19 23:14:01 +0200

    Do not remove stuff from build

--- setup.sh
+++ setup.sh
@@ -2,13 +2,8 @@
 
 
 THIS_DIR="$(readlink -f $(dirname $0))"
-
-if [ -d $THIS_DIR/build ]; then
-	rm -rf $THIS_DIR/build
-fi
-mkdir -p $THIS_DIR/build
-chmod 777 -R $THIS_DIR/build
 docker run -ti \
 	--volume "$THIS_DIR/:$THIS_DIR" \
+	--privileged \
 	-w $THIS_DIR \
 	yocto:3.1 $THIS_DIR/init/gosu 1000 /bin/bash
